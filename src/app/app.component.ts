c65e3aa6b8955c29fe9fbc28a8dd2d3a051436b4
Cleiton Santos <cleitonstos@gmail.com>
2023-01-09 11:16:26 +0044

    Criando Repositório no GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-course-carsales3';
+  title = 'Bem Vindo ao Curso de Frontend com Angular';
 }
